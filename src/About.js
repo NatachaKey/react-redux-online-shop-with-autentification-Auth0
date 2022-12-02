461ce18efd3fd7df5ab8aba48d1d6a3f4ccddf34
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-12-02 17:43:34 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,33 +1,23 @@
 import video from './VideoOne.mp4';
 import React from 'react';
-
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 AOS.init({
     duration: 1200,
   })
 
-
-
 const About = ()=>{
-    return(
-    
-            <div >
+    return(   
+        <div >
             <video autoPlay muted loop className='background-video'>
-                <source src={video} type="video/mp4" />
-                
+                <source src={video} type="video/mp4" />               
             </video>
-            <div className='aboutLines'>
-              
+            <div className='aboutLines'>            
                <p className='par' data-aos="fade-up">CANDYSHOP</p>
                <p className='par' data-aos="fade-up">Your favourite genuine sweets</p>
-            <p className='par italic' data-aos="fade-up">Since 1948</p>  
-            </div>
-           
-        </div>
-       
-      
-    
+               <p className='par italic' data-aos="fade-up">Since 1948</p>  
+            </div>       
+        </div> 
     )
 }
 
