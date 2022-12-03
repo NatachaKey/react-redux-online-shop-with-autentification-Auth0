3b68b51b32d072fb12dc41bb5b933259b49760dd
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-12-03 17:59:30 +0100

    Update AllCategories.js

--- src/Components/Filter/AllCategories.js
+++ src/Components/Filter/AllCategories.js
@@ -3,9 +3,7 @@ import Filter from "./Filter";
 const AllCategories = ()=> {
     return(<div>
         <h1 className="bgCart"> YOUR CART</h1>
-        <h2 className="italic">Smth sweety this way comes...</h2>
-        
-        
+        <h2 className="italic">Smth sweety this way comes...</h2>        
         { ['ALL','COOKIES', 'CAKES', 'MUFFINS', 'CREPES'].map((category, index)=> <Filter key={index} category={category}/>)}
         
     </div>)
